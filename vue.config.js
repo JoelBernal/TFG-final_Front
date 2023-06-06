a4059f60b5769e78379927705fb4d1c84cbd7e7a
Enrique Begué Aguirre <a25616@svalero.com>
2023-06-06 22:58:58 +0100

    por favor dime que funcionas

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,22 @@
 module.exports = {
   transpileDependencies: [
     'vuetify'
-  ]
-}
+  ],
+  configureWebpack: {
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules\/(?!chart.js)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+            },
+          },
+        },
+      ],
+    },
+  },
+};
+
